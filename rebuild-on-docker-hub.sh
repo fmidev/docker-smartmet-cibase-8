d34d407a56580e9508805a1dc9dd78e52abe69b8
Heikki Pernu <heikki.pernu@nekonet.fi>
2018-10-05 13:08:06 +0300

    Rebuild on docker script

---
+++ rebuild-on-docker-hub.sh
@@ -0,0 +1,5 @@
+#!/bin/sh 
+
+set -ex
+# Force rebuild on docker hub
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/fmidev/smartmet-cibase/trigger/eae5f518-2c1e-4d5b-9b18-b8abc52c8acd/ 
