cea6f617d4715c1fc57d365a5db66689121f44a9
Heikki Pernu <heikki.pernu@nekonet.fi>
2018-10-09 19:17:23 +0300

    Add gid mapping in addition to uid

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 targetuid=`id -u`
+targetgid=`id -g`
 
 # Execute various prepartion steps when running something inside the container
 if [ "$targetuid" != "0" -a "$targetuid" != "rpmbuild" ] ; then
 	# Create a user for this user and make home available
-	gosu 0 useradd -m -u $targetuid -s /bin/bash u$targetuid
+	gosu 0 groupadd -o -g 20 g$targetgid
+	gosu 0 useradd -o -m -u $targetuid -g $targetgid -s /bin/bash u$targetuid
 	HOME=/home/u$targetuid
 	export HOME
 fi
@@ -13,4 +15,4 @@ fi
 "$@"
 
 # Make sure certain file permissions are ok on host system
-if [  "$targetuid" != "0" ] ; then sudo chown -R $targetuid /var/cache/yum /ccache ; fi
+if [  "$targetuid" != "0" ] ; then sudo chown -R $targetuid.$targetgid /var/cache/yum /ccache ; fi
