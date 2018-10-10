c472c7cdf2132e71eeca9e89702218c2aedf1a9b
Heikki Pernu <heikki.pernu@nekonet.fi>
2018-10-10 12:56:42 +0300

    Make ci-build work again

--- ci-build.sh
+++ ci-build.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
-# Go to git repo root
-cd `dirname $0`
-cd ..
-
 function insudo {
     user=`whoami`
     if [ "$user" = "root" ] ; then
-	"$@"
-	return
+		"$@"
+		return $?
     fi
     if [ ! -x /usr/bin/sudo ] ; then
-	echo "Sudo not installed and installation not possible as regular user"
-	exit 1
+		echo "Sudo not installed and installation not possible as regular user"
+		exit 1
     fi
     /usr/bin/sudo "$@"
+    return $?
 }
 
+function usage { 
+	echo "usage: `basename $0` step [step] ..." >&2
+	echo "where steps are executed in order given and might be one of:" >&2
+	echo "  fmiprep  Install FMI repositories and related tools. Redundant if correct docker image is in use." >&2
+	echo "  prep     Prepare for building, generally installation of dependencies" >&2
+	echo "  build    Run make (in FMI projects generally does not build the RPM, CI usually wants RPM)" >&2
+	echo "  rpm      Build rpm and move over to directory defined by DISTDIR" >&2
+	echo "  testprep Prepare for testing i.e. install dependencies and link library files to test dirs" >&2
+	echo "  test     Run make test" >&2
+	exit 1
+}
+
+# Number of jobs to use in make
 jobs=`fgrep processor /proc/cpuinfo | wc -l`
+
+# Help
+if [ "$#" -lt "1" ] ; then usage ; fi
+
+# Search for the root of build tree but stop when in system root
+depthlimit=20
+while [ ! -d .git -a "$depthlimit" -gt "0"  ] ; do
+	cd ..
+	depthlimit=`expr $depthlimit - 1`
+done
+if [ ! -d .git ] ; then
+	echo "This is not a git source tree"
+	exit 1
+fi
+echo "Source tree base is in `pwd`"
+echo "Git origin is `git remote get-url origin`"
+
 
 # Try to find/create suitable directory for build time distribution files
 if [ -z "$DISTDIR" ] ; then
@@ -48,26 +75,24 @@ for step in $* ; do
 	    insudo yum update -y
 	    ;;
 	fmiprep)
-		for pck in http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/epel-release-latest-7.noarch.rpm \
-			yum-utils \
-		    git \
-		    ccache \
-			https://download.fmi.fi/smartmet-open/rhel/7/x86_64/smartmet-open-release-17.9.28-1.el7.fmi.noarch.rpm \
-			https://download.fmi.fi/fmiforge/rhel/7/x86_64/fmiforge-release-17.9.28-1.el7.fmi.noarch.rpm \
-			https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-redhat95-9.5-3.noarch.rpm ; do
-			insudo yum install -y "$pck" || insudo yum reinstall -y "$pck"
-		done
 	    # This will speedup future steps and there seems to be
 	    # wrong URLs in these in some cases
 	    insudo rm -f /etc/yum.repos.d/CentOS-Vault.repo /etc/yum.repos.d/CentOS-Sources.repo
+	    # Install various packages if needed
+		command -v yum-builddep 2>/dev/null || insudo yum install -y yum-utils
+		command -v git 2>/dev/null || insudo yum install -y git
+		command -v ccache 2>/dev/null || insudo yum install -y ccache
+		test -e /etc/yum.repos.d/epel.repo 2>/dev/null || insudo yum install -y http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/epel-release-latest-7.noarch.rpm
+		test -e /etc/yum.repos.d/smartmet-open.repo || insudo yum install -y https://download.fmi.fi/smartmet-open/rhel/7/x86_64/smartmet-open-release-17.9.28-1.el7.fmi.noarch.rpm
+		test -e /etc/yum.repos.d/fmiforge.repo || insudo yum install -y https://download.fmi.fi/fmiforge/rhel/7/x86_64/fmiforge-release-17.9.28-1.el7.fmi.noarch.rpm
+		test -e /etc/yum.repos.d/pgdg-95-redhat.repo || insudo yum install -y https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-redhat95-9.5-3.noarch.rpm
 	    # Enable shared C Cache if enabled by surrounding environment(i.e. localbuild)
 	    test ! -d "/ccache/." || (
 	    	echo cache_dir=/ccache > /etc/ccache.conf
 	    	echo umask=006 >> /etc/ccache.conf
-	    	ln -s /usr/bin/ccache /usr/local/bin/c++ && \
-    		ln -s /usr/bin/ccache /usr/local/bin/g++ && \
-   			ln -s /usr/bin/ccache /usr/local/bin/gcc && \
-	    	ln -s /usr/bin/ccache /usr/local/bin/cc
+	    	for i in c++ g++ gcc cc ; do
+	    		test -e /usr/local/bin/$i || insudo ln -s /usr/bin/ccache /usr/local/bin/$i
+	    	done
 	    )
 	    ccache -s
 	    ;;
