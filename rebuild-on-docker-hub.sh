86aed4545aa703c7fec3fb70aed9bd3c30b24efc
Heikki Pernu <pernu@fmi.fi>
2019-02-28 16:54:57 +0200

    Actually check return status of rebuild request

--- rebuild-on-docker-hub.sh
+++ rebuild-on-docker-hub.sh
@@ -1,6 +1,22 @@
-#!/bin/sh 
+#!/bin/sh -e
 
-set -ex
-# Force rebuild on docker hub
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/fmidev/smartmet-cibase/trigger/eae5f518-2c1e-4d5b-9b18-b8abc52c8acd/ 
-echo
+tmpfile=`mktemp`
+trap "rm -f $tmpfile" EXIT
+
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/fmidev/smartmet-cibase/trigger/eae5f518-2c1e-4d5b-9b18-b8abc52c8acd/ >"$tmpfile"
+
+if command -v json_reformat >/dev/null ; then
+ 	json_reformat < "$tmpfile"
+else 
+	cat "$tmpfile"
+fi
+
+# Check status
+if fgrep -q '"state": "Success"' "$tmpfile" ; then
+	echo 'Rebuild on docker hub initiated succesfully'
+	echo "NOTE: Rebuild may take several minutes before new version is downloadable"
+else
+	echo 'Rebuild start on docker hub failed!'
+	false
+fi
+ 
