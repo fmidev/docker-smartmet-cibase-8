7a9312ebf25354f17979210103a1151b3615dade
Heikki Pernu <pernu@fmi.fi>
2019-02-28 14:21:02 +0200

    Fix gid mapping

--- wrapper.sh
+++ wrapper.sh
@@ -13,7 +13,7 @@ fi
 # Execute various prepartion steps when running something inside the container
 if [ "$targetuid" != "0" ] ; then
 	# Create a user for this user and make home available
-	gosu 0 groupadd -o -g 20 g$targetgid
+	gosu 0 groupadd -o -g $targetgid g$targetgid
 	gosu 0 useradd -o -m -u $targetuid -g $targetgid -s /bin/bash u$targetuid
 	HOME=/home/u$targetuid
 	export HOME
