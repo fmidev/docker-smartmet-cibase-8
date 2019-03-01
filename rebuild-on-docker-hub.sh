45bc5d1a5d1f3c4f90d3c7e5dc892732fa112728
Heikki Pernu <heikki.pernu@nekonet.fi>
2019-03-01 11:41:08 +0200

    Disable rebuild retry, may initiate too many builds

--- rebuild-on-docker-hub.sh
+++ rebuild-on-docker-hub.sh
@@ -7,11 +7,13 @@ rebuilding="yes"
 
 while [ "$rebuilding" ] ; do
     curl -H "Content-Type: application/json" --data '{"build": true}' -X POST "https://cloud.docker.com/api/build/v1/source/ea31c6b5-8ca1-4d0f-bd03-49eedd44c7f7/trigger/356f5bd2-1b0d-4fbf-a502-f706b6e7910d/call/" >"$tmpfile"
-    rebuilding=`fgrep '"state": "Building"' < $tmpfile`
-    if [ "$rebuilding" ] ; then
-        echo "Still building previous version, waiting to retrigger build"
-        sleep 60
-    fi
+    rebuilding=""
+# Rebuild automation does not appear to quite work 
+#    rebuilding=`fgrep '"state": "Building"' < $tmpfile`
+#    if [ "$rebuilding" ] ; then
+#        echo "Still building previous version, waiting to retrigger build"
+#        sleep 60
+#    fi
 done
 
 json_reformat < "$tmpfile" || cat "$tmpfile"
@@ -19,7 +21,10 @@ json_reformat < "$tmpfile" || cat "$tmpfile"
 # Check status
 if fgrep -q '"state": "Success"' "$tmpfile" ; then
 	echo 'Rebuild on docker hub initiated succesfully'
-	echo "NOTE: Rebuild may take several minutes before new version is downloadable"
+	echo "NOTE: Build may take several minutes before new version is downloadable"
+elif fgrep -q '"state": "Building"' "$tmpfile" ; then
+    echo 'Rebuild on docker hub running'
+    echo "NOTE: Build may take several minutes before new version is downloadable"
 else
 	echo 'Rebuild start on docker hub failed!'
 	false
